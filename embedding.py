a6ca9bddd291d9c719916636076774f650a12150
Manpreet Kaur <jassimanpreet921@gmail.com>
2026-03-13 15:07:39 +0530

    Save all project updates

--- embedding.py
+++ embedding.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import torch
 from facenet_pytorch import InceptionResnetV1
 from PIL import Image
@@ -29,36 +28,4 @@ def get_embedding(face_img):
     with torch.no_grad():
         embedding = model(tensor)
 
-=======
-import torch
-from facenet_pytorch import InceptionResnetV1
-from PIL import Image
-import numpy as np
-
-# Select device (GPU if available, else CPU)
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-# Load FaceNet model once (important for performance)
-model = InceptionResnetV1(pretrained="vggface2").eval().to(device)
-
-
-def get_embedding(face_img):
-    """
-    Convert detected face image into a 512-D face embedding
-    """
-
-    # Resize face image
-    img = Image.fromarray(face_img).resize((160, 160))
-
-    # Normalize image
-    img = np.array(img).astype("float32") / 255.0
-
-    # Convert to tensor
-    tensor = torch.tensor(img).permute(2, 0, 1).unsqueeze(0).to(device)
-
-    # Generate embedding
-    with torch.no_grad():
-        embedding = model(tensor)
-
->>>>>>> d7aab021af56edae4ca8a7650116415220551368
     return embedding.cpu().numpy()[0]
--- similarity.py
+++ similarity.py
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 import numpy as np
 
 def cosine_similarity(a, b):
-=======
-import numpy as np
 
-def cosine_similarity(a, b):
->>>>>>> d7aab021af56edae4ca8a7650116415220551368
     return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
